4d6f2bcab5c93991f1b639a4503db7f836770738
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-09-24 12:57:49 +0400

    context providers

--- src/app/components/imageSlider.tsx
+++ src/app/components/imageSlider.tsx
@@ -4,7 +4,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-export default function ServerRoutePage(){
+export const  ImageSlider = ()=>{
     const settings = {
     dots: true,
   };
