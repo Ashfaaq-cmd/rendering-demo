0b655413370b6d834ee331810e2befb621e11094
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-09-24 13:15:40 +0400

    client on;y code

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { useTheme } from '../components/theme-provider';
+import { clientSideFunction } from '@/utils/client-utils';
 
 export default function ClientRoutePage(){
   const theme = useTheme()
+  const result = clientSideFunction()
   return (
+    <div>
     <h1 style={{ color: theme.colors.primary }}>Client Router page</h1>
+    <p>{result}</p>
+    </div>
   );
 }
--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { ImageSlider } from "../components/imageSlider";
 import { serverSideFunction } from "@/utils/server-utils";
+import { clientSideFunction } from '@/utils/client-utils';
+
 
 export default function ServerRoutePage(){
     const result = serverSideFunction();
+        const clientResult = clientSideFunction();
+
     return(
         <>
-        <h1>Server Route {result}</h1>
+        <h1>Server Route {result} {clientResult}</h1>
          <ImageSlider />;
          </>
     );
