d795708dcc285d6016df77a5a0dee8e8bb7106a4
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-09-24 12:08:30 +0400

    third party packages

---
+++ src/utils/server-utils.ts
@@ -0,0 +1,6 @@
+export const serverSideFunction = () =>{
+    console.log(
+        'use multiple libraries, use environment variables, interact with a database, process confidential information'
+    );
+    return "server result";
+};
